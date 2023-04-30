07121709c6faa2dc5f22849af925e51148a9a872
sean <seanschmidt89@yahoo.com>
2023-04-29 23:07:18 -0400

    setting up new proj

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import SideNav from "./components/SideNav/SideNav";
+import Hero from "./components/Hero/Hero";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <SideNav />
+      <Hero />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SideNav/SideNav.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./SideNav.css";
+
+const SideNav = () => {
+  return <div>SideNav</div>;
+};
+
+export default SideNav;
