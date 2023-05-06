e857ab7599b81a327510257c0478e7b616175e8f
sean <seanschmidt89@yahoo.com>
2023-05-05 23:53:01 -0400

    adding benefits img

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import SideNav from "./components/SideNav/SideNav";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
+import Benefits from "./components/Benefits/Benefits";
 
 function App() {
   return (
     <div className="App">
       <SideNav />
       <Hero />
+      <Benefits />
       <About />
     </div>
   );
---
+++ src/components/Benefits/Benefits.js
@@ -0,0 +1,13 @@
+import React from "react";
+import BenefitsImg from "../../assests/hero3.jpg";
+import "./Benefits.css";
+
+const Benefits = () => {
+  return (
+    <div className="benefits" id="benefits">
+      <img src={BenefitsImg} alt="/" />
+    </div>
+  );
+};
+
+export default Benefits;
