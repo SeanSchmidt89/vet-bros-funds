2eea22f945e3a5189c1c6a6da4242deed970592c
sean <seanschmidt89@yahoo.com>
2023-05-08 23:30:54 -0400

    adding contact form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,7 +2,18 @@ import React from "react";
 import "./Contact.css";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="contact">
+      <img src="/" alt="/" />
+      <div className="overlay" />
+      <div className="form-container">
+        <form>
+          <input />
+          <button>send message</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
