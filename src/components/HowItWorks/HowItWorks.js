e3062d998304c7c68271288de171b7f5efb86e11
sean <seanschmidt89@yahoo.com>
2023-06-07 21:48:09 -0400

    adding side markers to title

--- src/components/HowItWorks/HowItWorks.js
+++ src/components/HowItWorks/HowItWorks.js
@@ -8,12 +8,17 @@ const HowItWorks = () => {
       <img src={HowItWorksImg} alt="/" />
       <div className="overlay" />
       <div className="howitworks-text">
-        <h1>How It Works</h1>
+        <div className="mark-container">
+          <h1>How It Works</h1>
+          <div className="left-mark" />
+          <div className="right-mark" />
+        </div>
+
         <p>
           We offer comprehensive solutions for ATMs. We supply the ATM, we
           service it, refill it, and do the installation ourselves. Our ATMs are
           equipped with wireless internet modems, so we won’t have to connect to
-          your internet, we just need an outlet for power .Every time the
+          your internet, we just need an outlet for power. Every time the
           customer pulls money from the ATM you will be paid a percentage. The
           more customers that pull-out money from the ATM the more you are
           getting paid and the less you are paying the processing companies
