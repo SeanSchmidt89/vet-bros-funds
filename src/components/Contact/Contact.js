14efcc3f3a605f9c1ac742718e09892803561048
sean <seanschmidt89@yahoo.com>
2023-06-15 00:06:02 -0400

    adding new form handlers and state

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,10 +6,27 @@ const Contact = () => {
   const [name, setName] = useState("");
   const [number, setNumber] = useState("");
   const [message, setMessage] = useState("");
+  const [validation, setValidation] = useState(false);
+
+  const nameHandler = (e) => {
+    setName(e.target.value);
+  };
+
+  const numberHandler = (e) => {
+    setNumber(e.target.value);
+  };
+
+  const messageHandler = (e) => {
+    setMessage(e.target.value);
+  };
 
   const formHandler = (e) => {
     e.preventDefault();
-    console.log(e.target.value);
+    setName("");
+    setNumber("");
+    setMessage("");
+    setValidation(true);
+    console.log(name, number, message);
   };
   return (
     <div className="contact" id="contact">
@@ -18,10 +35,15 @@ const Contact = () => {
       <div className="form-container">
         <form className="form-container-two" onSubmit={formHandler}>
           <h1>Contact Us</h1>
-          <input placeholder="Name" />
-          <input placeholder="Number" />
-          <textarea placeholder="Message" />
+          <input onChange={nameHandler} value={name} placeholder="Name" />
+          <input onChange={numberHandler} value={number} placeholder="Number" />
+          <textarea
+            onChange={messageHandler}
+            value={message}
+            placeholder="Message"
+          />
           <button>SEND MESSAGE</button>
+          {validation ? <p className="validation">Mesage Sent</p> : null}
         </form>
       </div>
     </div>
